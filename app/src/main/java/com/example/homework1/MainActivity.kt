ce0cbe9eef46ffd65fa85ae8ac08efbc4a06871a
9eofrey <matviytka3chenko@gmail.com>
2023-10-30 21:04:18 +0200

    added registration and validation

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.homework1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        setContentView(R.layout.activity_main)
+        var textuser = findViewById<TextView>(R.id.profile_name_text)
+
+        val intent = intent.getStringExtra(Constants.username)
+        textuser.text= intent
+
+
     }
 }
