f3d75f9d10b305bfb9d94568590811a62176a28f
9eofrey <matviytka3chenko@gmail.com>
2023-10-31 21:54:10 +0200

    added data parsing

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.homework1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter.AllCaps
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +13,14 @@ class MainActivity : AppCompatActivity() {
         var textuser = findViewById<TextView>(R.id.profile_name_text)
 
         val intent = intent.getStringExtra(Constants.username)
-        textuser.text= intent
+
+val parsed = intent?.substringBefore("@")
+val parsedName= intent!!.substringBefore(".").capitalize()
+val parsedSurname =parsed!!.substringAfter(".").capitalize()
+
+val parsedAccountName= parsedName +" "+ parsedSurname
+
+        textuser.text= parsedAccountName
 
 
     }
